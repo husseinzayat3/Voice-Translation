5eb5b0180a93ec849471f0dd55d87d0e1a295834
hussein zayat <husseinzayat10@hotmail.com>
2020-08-11 12:47:10 +0200

    Select base language to be recorded

--- lib/Pages/RecordingPage.dart
+++ lib/Pages/RecordingPage.dart
@@ -25,6 +25,35 @@ class _RecordingPageState extends State<RecordingPage> {
 
   bool recordingDone = false;
 
+  bool _hasSpeech = false;
+  double level = 0.0;
+  double minSoundLevel = 50000;
+  double maxSoundLevel = -50000;
+  String lastWords = "";
+  String lastError = "";
+  String lastStatus = "";
+  String _currentLocaleId = "";
+  String _baseLocaleId = "";
+  List<stt.LocaleName> _localeNames = [];
+
+  Future<void> initSpeechState() async {
+    bool hasSpeech = await speech.initialize(
+        onError: errorListener, onStatus: statusListener);
+    if (hasSpeech) {
+      _localeNames = await speech.locales();
+
+      var systemLocale = await speech.systemLocale();
+      _currentLocaleId = systemLocale.localeId;
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _hasSpeech = hasSpeech;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
 
@@ -37,6 +66,26 @@ class _RecordingPageState extends State<RecordingPage> {
 //        mainAxisAlignment: MainAxisAlignment.center,
       shrinkWrap: true,
         children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+          child:Text("Select your language",textAlign: TextAlign.center,),),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              DropdownButton(
+                onChanged: (selectedVal) => _baseLang(selectedVal),
+                value: _baseLocaleId,
+                items: _localeNames
+                    .map(
+                      (localeName) => DropdownMenuItem(
+                    value: localeName.localeId,
+                    child: Text(localeName.name),
+                  ),
+                )
+                    .toList(),
+              ),
+            ],
+          ),
           Container(
           alignment: Alignment.topCenter,
           width: 50,
@@ -52,7 +101,7 @@ class _RecordingPageState extends State<RecordingPage> {
               available = await speech.initialize(
                   onStatus: statusListener, onError: errorListener,debugLogging: true);
               if (available) {
-                speech.listen(onResult: resultListener);
+                speech.listen(onResult: resultListener,localeId: _baseLocaleId);
               } else {
                 print("The user has denied the use of speech recognition.");
               }
@@ -73,12 +122,32 @@ class _RecordingPageState extends State<RecordingPage> {
               speech.stop();
             },
           )),
-          recordingDone?FlatButton(
+          recordingDone?
+          Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    DropdownButton(
+                      onChanged: (selectedVal) => _switchLang(selectedVal),
+                      value: _currentLocaleId,
+                      items: _localeNames
+                          .map(
+                            (localeName) => DropdownMenuItem(
+                          value: localeName.localeId,
+                          child: Text(localeName.name),
+                        ),
+                      )
+                          .toList(),
+                    ),
+                  ],
+                ),
+          FlatButton(
             child: Text("Translate"),
             onPressed: (){
               Navigator.push(context, new MaterialPageRoute(builder: (context) => TranslationPage(text:text)));
             },
-          ):SizedBox()
+          )]):SizedBox()
         ],
       ),
     );
@@ -92,6 +161,10 @@ class _RecordingPageState extends State<RecordingPage> {
   void initState() {
     super.initState();
 //    setupRecording();
+    if(!_hasSpeech){
+    initSpeechState();
+    }
+
   }
 
   void setupRecording() async{
@@ -107,5 +180,17 @@ class _RecordingPageState extends State<RecordingPage> {
     });
 
 
+  }
+  _switchLang(selectedVal) {
+    setState(() {
+      _currentLocaleId = selectedVal;
+    });
+    print(selectedVal);
+  }
+  _baseLang(selectedVal) {
+    setState(() {
+      _baseLocaleId = selectedVal;
+    });
+    print(selectedVal);
   }
 }
